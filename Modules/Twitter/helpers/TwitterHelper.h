02ab55d6753f13a08f68a3d45424a60549928825
Paulo Guilherme <uauker@gmail.com>
2013-09-05 13:38:23 -0300

    removido codigo nao utilizado

--- Modules/Twitter/helpers/TwitterHelper.h
+++ Modules/Twitter/helpers/TwitterHelper.h
@@ -9,12 +9,6 @@
 #import <Foundation/Foundation.h>
 #import "STTwitter.h"
 
-#define K_TWITTER_CONSUMER_NAME @"Trânsito Rio"
-#define K_TWITTER_CONSUMER_KEY @"WPDngmoDgU7SgRinKG5A"
-#define K_TWITTER_CONSUMER_SECRET @"2fbdQ69bdHAkhRLsrLKjj9gvdtdxJsKB7jgosO45nE"
-#define K_TWITTER_ACCESS_TOKEN @"25685569-SSMXlJ5SQqQ56u1DuedEDyikdQG9Uxc6y6hWGJ7gg"
-#define K_TWITTER_ACCESS_TOKEN_SECRET @"Vuc8NqLOwFXLVcKGphKKuh3XZnc8M5vEj61VQMEpWQ"
-
 @interface TwitterHelper : NSObject {
 }
 
