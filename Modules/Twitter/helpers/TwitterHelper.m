5f567c348f9c5ba26bc3e27a821bbc6fc3a4ee35
Paulo Guilherme <uauker@gmail.com>
2013-09-05 13:17:06 -0300

    organizando melhor o codigo

--- Modules/Twitter/helpers/TwitterHelper.m
+++ Modules/Twitter/helpers/TwitterHelper.m
@@ -15,23 +15,26 @@ static TwitterHelper* _instance;
 + (TwitterHelper *) sharedInstance {
     if (!_instance) {
         _instance = [[TwitterHelper alloc] init];
+        [_instance setKeys];
     }
     
     return _instance;
 }
 
 - (STTwitterAPI *)api {
-    [self setKeys];
-    
-    return [STTwitterAPI twitterAPIWithOAuthConsumerName:K_TWITTER_CONSUMER_NAME consumerKey:K_TWITTER_CONSUMER_KEY consumerSecret:K_TWITTER_CONSUMER_SECRET oauthToken:K_TWITTER_ACCESS_TOKEN oauthTokenSecret:K_TWITTER_ACCESS_TOKEN_SECRET];
+    return [STTwitterAPI twitterAPIWithOAuthConsumerName:self.consumerName consumerKey:self.consumerKey consumerSecret:self.consumerSecret oauthToken:self.accessToken oauthTokenSecret:self.accessTokenSecret];
 }
 
 - (void)setKeys {
-    NSString *bundle = [[NSBundle mainBundle] bundleIdentifier];
+//    NSString *bundle = [[NSBundle mainBundle] bundleIdentifier];
     
-    if ([bundle isEqualToString:@"com.uauker.apps.transitorio"]) {
-        
-    }
+//    if ([bundle isEqualToString:@"com.uauker.apps.transitorio"]) {
+        self.consumerName = @"Trânsito Rio";
+        self.consumerKey = @"WPDngmoDgU7SgRinKG5A";
+        self.consumerSecret = @"2fbdQ69bdHAkhRLsrLKjj9gvdtdxJsKB7jgosO45nE";
+        self.accessToken = @"25685569-SSMXlJ5SQqQ56u1DuedEDyikdQG9Uxc6y6hWGJ7gg";
+        self.accessTokenSecret = @"Vuc8NqLOwFXLVcKGphKKuh3XZnc8M5vEj61VQMEpWQ";
+//    }
 }
 
 @end
